b8d7819788cd1c535d1fa106bfd30809e0c9ed5d
liuyanhu <2632106754@qq.com>
2020-01-06 07:37:27 +0800

    ...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React,{Component} from 'react'
-
+import {Route,Switch,} from 'react-router-dom'
+import Disc from './containers/disc/disc'
 export default class App extends Component{
   render(){
     return (
-      <div>123</div>
+      <div className="app">
+        <Switch>
+          <Route path="/Disc" component={Disc}/>
+        </Switch>
+      </div>
     )
   }
 }
--- src/config/menu_config.js
+++ src/config/menu_config.js
@@ -1,3 +1,8 @@
 export default[
-  
+  {
+    title: '首页', // 菜单标题名称
+    key: 'home', // 对应的path
+    icon: 'home', // 图标名称
+    path: '/admin/home'//对应路径
+  },
 ]
