19cf41b461aa84e55bc4a8aa781cc39da0f6f3de
zhangyahui <1733594272@qq.com>
2020-01-06 11:56:04 +0800

    电台分类静态页面

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React,{Component} from 'react';
 import './app.less';
 
 import Header from './components/Header/Header.jsx'
+import Radio from './containers/Radio/Radio'
 export default class App extends Component{
   render(){
     return (
       <div className="kjcAppContainer">
-        <Header/>
+        {/* <Header/> */}
+        <Radio/>
       </div>
     )
   }
