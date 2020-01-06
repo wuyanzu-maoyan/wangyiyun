ee39c0d4048b2ebffcf1f883d820409d4708fc7d
youhaoxia <1363356081@qq.com>
2020-01-06 10:00:24 +0800

    init

---
+++ src/config/artistMenuConfig.js
@@ -0,0 +1,21 @@
+export default[
+  {
+    title:'推荐',
+    key: '1',
+    children: [ // 子菜单列表
+      {
+        title: '推荐歌手',
+        key: 'category',
+        icon: 'unordered-list',
+        path: '/admin/prod_about/category'
+      },
+      {
+        title: '商品管理',
+        key: 'product',
+        icon: 'tool',
+        path: '/admin/prod_about/product'
+      },
+    ]
+
+  }
+]
