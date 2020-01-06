70655300dbbea240e4b5e667e66e3147b558299b
zss <1750592414@qq.com>
2020-01-06 14:59:54 +0800

    轮播图

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React,{Component} from 'react';
 import {Switch} from 'react-router-dom'
-import './app.less';
+import './App.less';
 
 import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import Recommend from './containers/Recommend/Recommend';
 export default class App extends Component{
   render(){
     return (
       <div className="kjcAppContainer">
         <Header/>
         <div className="kjcContent">
-          <Switch>
+          {/* <Switch>
 
-          </Switch>
+          </Switch> */}
         </div>
         <Footer/>
       </div>
