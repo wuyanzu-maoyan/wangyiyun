e374f90cb8c08f9cec26ec90f88c4501fa92ba56
youhaoxia <1363356081@qq.com>
2020-01-06 13:56:44 +0800

    add

--- src/config/artistMenuConfig.js
+++ src/config/artistMenuConfig.js
@@ -5,17 +5,137 @@ export default[
     children: [ // 子菜单列表
       {
         title: '推荐歌手',
-        key: 'category',
-        icon: 'unordered-list',
-        path: '/admin/prod_about/category'
+        key: '11',
+        id: '',
+        path: ''
       },
       {
-        title: '商品管理',
-        key: 'product',
-        icon: 'tool',
-        path: '/admin/prod_about/product'
+        title: '入驻歌手',
+        key: '12',
+        id: '5001',
+        path: ''
       },
     ]
 
-  }
+  },
+  {
+    title:'华语',
+    key: '2',
+    children: [ // 子菜单列表
+      {
+        title: '华语男歌手',
+        key: '21',
+        id: '1001',
+        path: ''
+      },
+      {
+        title: '华语女歌手',
+        key: '22',
+        id: '1002',
+        path: ''
+      },
+      {
+        title: '华语组合/乐队',
+        key: '23',
+        id: '1003',
+        path: ''
+      },
+    ]
+  },
+  {
+    title:'欧美',
+    key: '3',
+    children: [ // 子菜单列表
+      {
+        title: '欧美男歌手',
+        key: '31',
+        id: '2001',
+        path: ''
+      },
+      {
+        title: '欧美女歌手',
+        key: '32',
+        id: '2002',
+        path: ''
+      },
+      {
+        title: '欧美组合/乐队',
+        key: '33',
+        id: '2003',
+        path: ''
+      },
+    ]
+  },
+  {
+    title:'日本',
+    key: '4',
+    children: [ // 子菜单列表
+      {
+        title: '日本男歌手',
+        key: '41',
+        id: '6001',
+        path: ''
+      },
+      {
+        title: '日本女歌手',
+        key: '42',
+        id: '6002',
+        path: ''
+      },
+      {
+        title: '日本组合/乐队',
+        key: '43',
+        id: '6003',
+        path: ''
+      },
+    ]
+  },
+  {
+    title:'韩国',
+    key: '5',
+    children: [ // 子菜单列表
+      {
+        title: '韩国男歌手',
+        key: '51',
+        id: '7001',
+        path: ''
+      },
+      {
+        title: '韩国女歌手',
+        key: '52',
+        id: '7002',
+        path: ''
+      },
+      {
+        title: '韩国组合/乐队',
+        key: '53',
+        id: '7003',
+        path: ''
+      },
+    ]
+  },
+  {
+    title:'其他',
+    key: '6',
+    children: [ // 子菜单列表
+      {
+        title: '其他男歌手',
+        key: '61',
+        id: '4001',
+        path: ''
+      },
+      {
+        title: '其他女歌手',
+        key: '62',
+        id: '4002',
+        path: ''
+      },
+      {
+        title: '其他组合/乐队',
+        key: '63',
+        id: '4003',
+        path: ''
+      },
+    ]
+  },
 ]
