8d593c244ed272c756921afb05b093b55c7a1960
liuyanhu <2632106754@qq.com>
2020-01-07 18:35:19 +0800

    ..

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,10 @@
 
 import ajax from './myAxios'
-import { LIMIT,ORDER } from "../config/index";
 
 //获取歌手列表
 export const reqArtistList = (cat,initial) => ajax.get(`/artist/list?cat=${cat}&initial=${initial}`)
 //获取碟片信息
-export const reqAlbumList = () =>ajax.get(`/top/playlist?limit=${LIMIT}&order=${ORDER}`)
+export const reqAlbumList = () =>ajax.get('/top/album?order=new&limit=10&')
 
 //请求轮播图的歌曲
 export const reqMVlist = () => ajax.get('personalized/mv');
--- src/containers/Disc-DVD/Disc-DVD.jsx
+++ src/containers/Disc-DVD/Disc-DVD.jsx
@@ -15,8 +15,14 @@ import { reqAlbumList } from "../../api/index";
     this.getAlbumList()
   }
   getAlbumList = async()=>{
-    let result = await reqAlbumList()
-    console.log(result)
+    let data = await reqAlbumList()
+    console.log(data)
+    const { code, albums } = data
+  if (code===200) {
+    this.setState({ albumList: albums })
+  }
+    
+
   }
   render(){
     return (
@@ -27,55 +33,20 @@ import { reqAlbumList } from "../../api/index";
               <span>新碟上架</span>
             </h3>
           </div>
-          <ul className="cvrlst">
-            <li>
-              <div className="cover"><img src="http://p3.music.126.net/R5hmBoVZt0I56BPiVic1aA==/109951164601146483.jpg?param=130y130"></img>
-              <a href="/album?id=84698311" className="msk" title="Yummy"></a>
-              <a href="#" className="icon-play f-alpha f-fr" title="播放" data-res-type="19" data-res-id="84698311" data-res-action="play"></a>
-              </div>
-              <p className="dec"><a>Yummy</a></p>
-              <p className="thide"><span><a>Justin Bieber</a></span></p>
-            </li>
-            <li>
-              <div className="cover"><img src="http://p3.music.126.net/R5hmBoVZt0I56BPiVic1aA==/109951164601146483.jpg?param=130y130"></img>
-              <a href="/album?id=84698311" className="msk" title="Yummy"></a>
-              <a href="#" className="icon-play f-alpha f-fr" title="播放" data-res-type="19" data-res-id="84698311" data-res-action="play"></a>
-              </div>
-              <p className="dec"><a>Yummy</a></p>
-              <p className="thide"><span><a>Justin Bieber</a></span></p>
-            </li>
-            <li>
-              <div className="cover"><img src="http://p3.music.126.net/R5hmBoVZt0I56BPiVic1aA==/109951164601146483.jpg?param=130y130"></img>
-              <a href="/album?id=84698311" className="msk" title="Yummy"></a>
-              <a href="#" className="icon-play f-alpha f-fr" title="播放" data-res-type="19" data-res-id="84698311" data-res-action="play"></a>
-              </div>
-              <p className="dec"><a>Yummy</a></p>
-              <p className="thide"><span><a>Justin Bieber</a></span></p>
-            </li>
-            <li>
-              <div className="cover"><img src="http://p3.music.126.net/R5hmBoVZt0I56BPiVic1aA==/109951164601146483.jpg?param=130y130"></img>
-              <a href="/album?id=84698311" className="msk" title="Yummy"></a>
-              <a href="#" className="icon-play f-alpha f-fr" title="播放" data-res-type="19" data-res-id="84698311" data-res-action="play"></a>
-              </div>
-              <p className="dec"><a>Yummy</a></p>
-              <p className="thide"><span><a>Justin Bieber</a></span></p>
-            </li>
-            <li>
-              <div className="cover"><img src="http://p3.music.126.net/R5hmBoVZt0I56BPiVic1aA==/109951164601146483.jpg?param=130y130"></img>
-              <a href="/album?id=84698311" className="msk" title="Yummy"></a>
-              <a href="#" className="icon-play f-alpha f-fr" title="播放" data-res-type="19" data-res-id="84698311" data-res-action="play"></a>
-              </div>
-              <p className="dec"><a>Yummy</a></p>
-              <p className="thide"><span><a>Justin Bieber</a></span></p>
-            </li>
-            <li>
-              <div className="cover"><img src="http://p3.music.126.net/R5hmBoVZt0I56BPiVic1aA==/109951164601146483.jpg?param=130y130"></img>
-              <a href="/album?id=84698311" className="msk" title="Yummy"></a>
-              <a href="#" className="icon-play f-alpha f-fr" title="播放" data-res-type="19" data-res-id="84698311" data-res-action="play"></a>
-              </div>
-              <p className="dec"><a>Yummy</a></p>
-              <p className="thide"><span><a>Justin Bieber</a></span></p>
-            </li>
+          <ul className="cvrlst" >
+          {
+                this.state.albumList.map((item,index) => {
+              <li key="item.id">
+                <div className="cover"><img src="http://p3.music.126.net/R5hmBoVZt0I56BPiVic1aA==/109951164601146483.jpg?param=130y130"></img>
+                <a href="/album?id=84698311" className="msk" title="Yummy"></a>
+                <a href="#" className="icon-play f-alpha f-fr" title="播放" data-res-type="19" data-res-id="84698311" data-res-action="play"></a>
+                </div>
+                <p className="dec"><a>Yummy</a></p>
+                <p className="thide"><span><a>Justin Bieber</a></span></p>
+              </li>
+                })
+              }
+          
           </ul>
           <div className="title1">
             <h3>
